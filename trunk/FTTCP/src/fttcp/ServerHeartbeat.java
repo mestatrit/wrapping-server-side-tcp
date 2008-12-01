d18915caaf7982c1838b1c1b0fc4141f5b5b75e3
wisseyegh <wisseyegh@334d2e66-a29b-11dd-98aa-2b35b7097d33>
2008-12-01 18:03:55 +0000

    Created a new thread system so that server sends heartbeats in a separate thread.

---
+++ trunk/FTTCP/src/fttcp/ServerHeartbeat.java
@@ -0,0 +1,44 @@
+/*
+ * ServerHeartbeat.java
+ *
+ * Created on December 1, 2008, 5:40 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package fttcp;
+
+/**
+ *
+ * @author csugbe
+ */
+public class ServerHeartbeat extends Thread {
+    
+    Main m;
+    server s;
+
+    
+    /** Creates a new instance of ServerHeartbeat */
+    public ServerHeartbeat(Main main, server newServer) {
+        this.m = main;
+        this.s = newServer;
+    }
+    
+    
+     public void run(){
+        
+         while(true){
+             try{
+                this.sleep(5000);
+                
+            }catch(Exception e){}
+             
+             s.sendHeartbeat();
+             
+         }
+         
+     }
+     
+
+}
