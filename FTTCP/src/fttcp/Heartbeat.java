69e3d832d70d924f818c5733bc4673ffffb57d4a
wisseyegh <wisseyegh@334d2e66-a29b-11dd-98aa-2b35b7097d33>
2008-12-03 16:26:14 +0000

    Comments

--- FTTCP/src/fttcp/Heartbeat.java
+++ FTTCP/src/fttcp/Heartbeat.java
@@ -22,13 +22,9 @@ public class Heartbeat extends Thread {
     private Main m;     // A reference to the instance of Main under which this thread is running.
     logger thisLogger;  // A reference to the instance of logger that created this instance of Heartbeat
     private String sender;      // TODO: Redundant? ReadHeartbeat no longer needed?
-    private boolean serverAlive = true;     // Indicates whether the Heartbeat currently believes the server to be alive or not.
     private boolean currentBeat = true;     // This is set to true by the logger every time it receives a beat. Heartbeat knows if currentbeat is true, the server must be alive.
-    private boolean interactingWithClient = false;  // This variable indicates whether logger.interactWithClient() has been called, and so avoids it being called twice.
     boolean detectBeats = true;     // This variable indicates whether heartbeat should be detecting heartbeats, or should temporarily not look for them.
     boolean finished = false;       // This variable controls a while loop that ensures that Heartbeat's behaviour is repeated until the program is finished.
-    
-    // TODO: REDUNDANT? private byte heartbeatFlag = 1;
     
     /** 
      * Constructor
@@ -72,58 +68,24 @@ public class Heartbeat extends Thread {
                     // No beat arrived during sleeping.
                     System.out.println("HEARTBEAT: No beat arrived, setting serverAlive to false.");
                     thisLogger.setOperatingNormally(false);
-                    this.detectBeats = false;   // Stop checking for heartbeats until told to by the logger.
+                  
+                        this.detectBeats = false;   // Stop checking for heartbeats until told to by the logger.
+                    
+                    
                 }
            }
         }while(finished == false);    
     }
-    
-    
-    public boolean getServerAlive(){
-        return serverAlive;
-    }
-    
-    
-    public void setServerAlive(boolean temp){
-        serverAlive = temp;
-    }
-       
    
-    //TODO: Redundant?
-    /*private byte[] readHeartbeat(){
-        try{
-            while(true){
-                FilenameFilter filter = new LOGFileFilter();
-                File f = new File("loggerBuffer/receivedHearbeat");
-                String[] files = f.list(filter);
-                FileInputStream fileinputstream = new FileInputStream("loggerBuffer/receivedHeartbeat"+files[0]);
-                int numberBytes = fileinputstream.available();
-                byte[] bytearray = new byte[numberBytes];
-                fileinputstream.read(bytearray);
-                boolean hadDel = (new File(files[0]).delete());
-                //Find and set sender
-                int length  = files[0].length();
-                sender = files[0].substring(length-7,length-4);
-                return bytearray;
-            }          
-        }
-        catch(java.io.FileNotFoundException e){
-            return null;
-        } 
-        catch(java.io.IOException e){
-            return null;
-        } 
-    }*/
     
+    /*called by the logger when the server restarts to indicate
+    to the heartbeat class that heartbeat is back*/
     public void beat(){
         System.out.println("HEARTBEAT: Logger received heatbeat. Set currentBeat to TRUE.");
         this.currentBeat = true;
     }
-    
-    public void setInteractingWithClient(boolean newBoolean){
-        this.interactingWithClient = newBoolean;
-    }
-    
+
+    /*Method to set detectBeats variable from the logger class*/
     public void setDetectBeats(boolean newBoolean){
         this.detectBeats = newBoolean;
     }
